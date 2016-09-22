fc947a9f91d539e95e93fc4b4f77f9023f7f6166
danto123 <shylanto@gmail.com>
2016-09-22 17:16:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article_one', function (req, res) {
-  res.send('article_one request servered');
+  res.sendFile(path.join(__dirname, 'ui', 'article_one.html'));
 });
 
 app.get('/article_two', function (req, res) {
