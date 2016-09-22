29cd06eed68859abe63c0c398afa9f4ddc042ce4
danto123 <shylanto@gmail.com>
2016-09-22 17:33:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,38 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+function createTemplate(data){
+    var htmlTemplate=
+    `<html>
+    <head>
+        <title>
+            
+        </title>
+        <meta name="viewport" content ="width=device-width,initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <div class="container">
+            <div>
+                <a href="/">HOME</a>
+            </div>
+            <hr/>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+                <p>
+               Nature, in the broadest sense, is the natural, physical, or material world or universe. "Nature" can refer to the phenomena of the physical world, and also to life in general. The study of nature is a large part of science. Although humans are part of nature, human activity is often understood as a separate category from other natural phenomena.</p>
+            </div>
+        </div>
+    </body>
+</html>`;
 
+return htmlTemplate;
+} 
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
