26ad758fbe32936bb4d4fd4e17cbd3cc0a5f9ec9
danto123 <shylanto@gmail.com>
2016-10-06 23:32:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,10 +15,21 @@ img.onclick = function(){
 };
 
 var button= document.getElementById('counter');
-var counter=0;
+
 
 button.onclick=function(){
-  counter =counter +1;
+ /* counter =counter +1;
   var span=document.getElementById('count');
+  span.innerHTML=counter.toString();*/
+  
+  var request=new XMLHttpRequest();
+  request.onreadystatechange=function(){
+      if(request.readyState==XMLHttpRequest.DONE){
+          if(request.status==200){
+               var span=document.getElementById('count');
   span.innerHTML=counter.toString();
+          }
+      }
+      
+  };
 };
