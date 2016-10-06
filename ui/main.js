1a9d0061105b3b86fca822e866d81ce45000c977
danto123 <shylanto@gmail.com>
2016-10-07 00:13:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -44,7 +44,7 @@ button.onclick=function(){
 
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
-    var request= new XMLHttpRequset();
+    var request= new XMLHttpRequest();
     request.onreadystatechange=function(){
         if(request.readyState==XMLHttpRequest.DONE){
             if(request.status==200){
