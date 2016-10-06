5b939c97862f5d40fc66126841c168ed1f85f5d1
danto123 <shylanto@gmail.com>
2016-10-07 00:19:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -62,7 +62,7 @@ submit.onclick=function(){
     };
 var nameInput=document.getElementById('name');
 var name=nameInput.value;
-    request.open('GET',"http://danto123.imad.hasura-app.io/submit-name?name="+name,true);
+    request.open('GET',"http://danto123.imad.hasura-app.io/submit-name?name=" + name,true);
   request.send(null);
 };
 
