d63c59910c8efbeb556d73d1a4ad5c76d2462ce4
danto123 <shylanto@gmail.com>
2016-10-07 00:06:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -45,14 +45,24 @@ var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
-    var names=['name1','name2','name3'];
+    var request= new XMLHttpRequset();
+    request.onreadystatechange=function(){
+        if(request.readyState==XMLHttpRequest.DONE){
+            if(request.status==200){
+              var names=['name1','name2','name3'];
     var list='';
     for(var i=0;i<names.length;i++)
     {
-        list='<li>'+names[i]+'</li>';
-    }
-   var ul=document.getElementById('namelist');
+        list+='<li>'+ names[i] +'</li>';
+    }  
+    var ul=document.getElementById('namelist');
    ul.innerHTML=list;
+            }
+        }
+    }
+    
+    request.open('GET',"http://danto123.imad.hasura-app.io/submit-name?name="+name,true);
+  request.send(null);
 };
 
 
