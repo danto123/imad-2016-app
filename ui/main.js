b07f005dac4fc30179f5a2a612974846e90c3e75
danto123 <shylanto@gmail.com>
2016-10-06 23:08:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ console.log('Loaded!');
 var img= document.getElementById('image');
 var marginLeft=0;
 function moveRight(){
-    marginLeft=marginLeft+1;
+    marginLeft=marginLeft + 1;
     img.style.marginLeft= marginLeft + 'px';
 }
 
